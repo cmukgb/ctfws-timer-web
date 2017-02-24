1d008d7796f311ee945cdda7a8507747548116fc
Michael Murphy <mjmurphy@cmu.edu>
2017-02-24 04:35:49 -0500

    Fix check for password file on different versions of readlink

--- no_game.sh
+++ no_game.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-password_dir=$(dirname $(readlink -f $0))
-password_file=$password_dir/password.txt
-
-if [ ! -f $password_file ]
+# If script was symlinked, allow password.txt to be at target of link
+password_file=""
+if [ -f "password.txt" ]
+then
+  password_file="password.txt"
+else
+  if [ -h "$0" ] # Test for symlink
+  then
+    password_dir=$(dirname $(readlink "$0"))
+    password_file="$password_dir"/password.txt
+  fi
+fi
+if [ -z "$password_file" ]
 then
   echo "File password.txt must exist to run this command"
   exit 1
@@ -14,6 +23,6 @@ then
   echo Usage: "$0"
   exit 1
 else
-  mosquitto_pub -h kgb.club.cc.cmu.edu -u ctfwsmaster -P $(cat $password_file) -q 1 -r -t ctfws/game/config -m none
+  mosquitto_pub -h kgb.club.cc.cmu.edu -u ctfwsmaster -P $(cat "$password_file") -q 1 -r -t ctfws/game/config -m none
 fi
 
--- start_game.sh
+++ start_game.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-password_dir=$(dirname $(readlink -f $0))
-password_file=$password_dir/password.txt
-
-if [ ! -f $password_file ]
+# If script was symlinked, allow password.txt to be at target of link
+password_file=""
+if [ -f "password.txt" ]
+then
+  password_file="password.txt"
+else
+  if [ -h "$0" ] # Test for symlink
+  then
+    password_dir=$(dirname $(readlink "$0"))
+    password_file="$password_dir"/password.txt
+  fi
+fi
+if [ -z "$password_file" ]
 then
   echo "File password.txt must exist to run this command"
   exit 1
