019710b011be32a40c4049a97b72ab5e8629b1c7
Michael Murphy <mjmurphy@cmu.edu>
2017-10-27 01:50:29 -0700

    Add initial judges view
    Not all functionality yet implemented

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+
+from subprocess import call
 
 def index(request):
     return render(request, 'timer.html')
@@ -8,7 +10,30 @@ def index(request):
 def user_is_judge(user):
     return user.groups.filter(name='judges').exists()
 
+def get_args(post):
+    if 'command' not in post:
+        raise Error
+    com = post['command']
+
+    if com == 'no_game':
+        return 'scripts/no_game.sh'
+    elif com == 'end_game':
+        return 'scripts/end_game.sh'
+    elif com == 'set_flags_hidden':
+        return ['scripts/set_flags.sh', '?']
+    elif com == 'set_flags_number':
+        red = post['red_flags']
+        yellow = post['yellow_flags']
+        return ['scripts/set_flags.sh', red + ' ' + yellow]
+    else:
+        raise Error
+
 @user_passes_test(user_is_judge)
 def judge(request):
-    return HttpResponse("You're in!")
+    if request.method == 'POST':
+        args = get_args(request.POST)
+        call(args)
+        return HttpResponse()
+    else:
+        return render(request, 'judge.html')
 
