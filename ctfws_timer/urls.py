08483055ac83881990e5bfafeb7c968b2e29c486
Michael Murphy <mjmurphy@cmu.edu>
2017-10-29 05:25:17 -0700

    Improve login page and set logout link in judges
    Also set page titles

--- ctfws_timer/urls.py
+++ ctfws_timer/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     url(r'^login/$',
         auth_views.LoginView.as_view(template_name='login.html'),
         name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view()),
+    url(r'^logout/$',
+        auth_views.LogoutView.as_view(),
+        name='logout'),
     url(r'^', include('main.urls')),
 ]
 
