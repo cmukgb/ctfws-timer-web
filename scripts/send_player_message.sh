512cf819e9cb68644d7422c5ed05ab164cd19cc2
Michael Murphy <mjmurphy@cmu.edu>
2017-10-30 01:53:45 -0700

    Fix message printed by send_player_message.sh

--- scripts/send_player_message.sh
+++ scripts/send_player_message.sh
@@ -20,6 +20,6 @@ else
   fi
   m="$d $1"
   mosquitto_pub -h kgb.club.cc.cmu.edu -u ctfwsmaster -P $(cat "$password_file") -q 1 -r -t ctfws/game/message/player -m "$m"
-  echo "Sent message at $d"
+  echo "Sent player message at $d"
 fi
 
