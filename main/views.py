6152852f6b7ba0efa28783823e40d523993120a2
Michael Murphy <mjmurphy@cmu.edu>
2017-10-30 01:55:10 -0700

    Implement backend for remaining commands

--- main/views.py
+++ main/views.py
@@ -18,17 +18,40 @@ def user_is_judge(user):
 def get_args(post):
     com = post['command']
     if com == 'no_game':
-        return 'scripts/no_game.sh'
+        return ['scripts/no_game.sh']
     elif com == 'end_game':
-        return 'scripts/end_game.sh'
+        result = ['scripts/end_game.sh']
+        if 'hide_flags' in post and post['hide_flags'] == 'on':
+            result += ['--hide-flags']
+        if 'end_timestamp' in post and post['end_timestamp'] != '':
+            result += [post['end_timestamp']]
+        return result
     elif com == 'set_flags_hidden':
         return ['scripts/set_flags.sh', '?']
     elif com == 'set_flags_number':
         red = post['red_flags']
         yellow = post['yellow_flags']
         return ['scripts/set_flags.sh', red + ' ' + yellow]
+    elif com == 'send_message':
+        if post['message_type'] == 'both':
+            result = ['scripts/send_message.sh']
+        elif post['message_type'] == 'players':
+            result = ['scripts/send_player_message.sh']
+        elif post['message_type'] == 'jail':
+            result = ['scripts/send_jail_message.sh']
+        else:
+            return None
+        result += [post['message']]
+        if 'message_timestamp' in post and post['message_timestamp'] != '':
+            result += [post['message_timestamp']]
+        return result
+    elif com == 'start_game':
+        result = ['scripts/start_game.sh', post['num_flags'], post['game_num']]
+        if 'start_timestamp' in post and post['start_timestamp'] != '':
+            result += [post['start_timestamp']]
+        return result
     else:
-        return HttpResponse('Unknown command', status=500)
+        return None
 
 @user_passes_test(user_is_judge)
 def judge(request):
@@ -48,6 +71,8 @@ def judge(request):
                 return HttpResponse('Invalid data', status=500)
         else:
             args = get_args(request.POST)
+            if args is None:
+                return HttpResponse('Unknown command', status=500)
             call(args)
         return HttpResponse()
 
