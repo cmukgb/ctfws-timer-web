7dcb2586f5ffcef2583a81e96b349fe9e3917707
Michael Murphy <mjmurphy@cmu.edu>
2017-11-04 21:35:41 -0700

    Switch debug off

--- ctfws_timer/settings.py
+++ ctfws_timer/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if DEBUG:
