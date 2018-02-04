8ca1639d34d4e5a77a5549dff962b4b417aef51b
Michael Murphy <mjmurphy@cmu.edu>
2018-02-04 00:59:22 -0800

    Add Heap Analytics script

--- ctfws_timer/urls.py
+++ ctfws_timer/urls.py
@@ -18,14 +18,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from ctfws_timer import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$',
-        auth_views.LoginView.as_view(template_name='login.html'),
+        auth_views.LoginView.as_view(template_name='login.html',
+            extra_context={'DEBUG': settings.DEBUG}),
         name='login'),
     url(r'^logout/$',
         auth_views.LogoutView.as_view(),
         name='logout'),
     url(r'^', include('main.urls')),
 ]
-
